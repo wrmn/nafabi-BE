cc653751011246c0341f725ef443d6077d02c4c7
aditya-wrmn <aditya_wrmn@protonmail.com>
2020-11-09 21:45:39 +0700

    search filter

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   validates :title, :user_id, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }, presence: true
   belongs_to :user
+  scope :filter_by_title, lambda { |keyword|
+    where('lower(title) Like ?', "%#{keyword.downcase}%")
+  }
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,9 +1,19 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
+  
   test "should have a positive price" do
     product = products(:one)
     product.price = -1
     assert_not product.valid?
   end
+  
+  test "should filter products by name" do
+    assert_equal 2, Product.filter_by_title('tv').count
+  end
+
+  test "should filter by products by name and sort them" do
+    assert_equal [products(:another_tv), products(:one)], Product.filter_by_title('tv').sort
+  end
+
 end
